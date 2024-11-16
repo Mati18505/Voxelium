54020aae4b677f555ee440322e096ee722d240c6
Mateusz Stulczewski <mstulczewski@gmail.com>
2024-11-16 17:05:53 +0100

    Added config

---
+++ src/client/config.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+use bevy::reflect::TypePath;
+use bevy_common_assets::yaml::YamlAssetPlugin;
+
+#[derive(serde::Deserialize, bevy::asset::Asset, bevy::reflect::TypePath)]
+pub struct Config {
+    column_height: u32,
+    render_distance: u32,
+    seed: i32,
+    use_threading: bool,
+    block_ray_cast_increment: f32,
+
+    world_scale: u32,
+    chunk_size: u32,
+} 
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            column_height: 8,
+            render_distance: 10,
+            seed: 0,
+            use_threading: false,
+            block_ray_cast_increment: 0.5,
+            
+            world_scale: 1,
+            chunk_size: 16,
+        }
+    }
+}
