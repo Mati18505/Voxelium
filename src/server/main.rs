5b30fe05e39e0197e0d7be3527794ec616050287
Mateusz Stulczewski <mstulczewski@gmail.com>
2024-11-16 17:05:30 +0100

    Moved main to server.

---
+++ src/server/main.rs
@@ -0,0 +1,26 @@
+use cgmath::Vector3;
+use project_craft::chunk::Chunk;
+use project_craft::{terrain_generator, world};
+use project_craft::types::ChunkPos;
+use project_craft::chunk_loader;
+
+fn main() {
+    let mut world = world::World::new();
+    let pos = ChunkPos(Vector3::<isize>::new(0, 0, 0));
+    world.add_chunk(ChunkPos(pos.clone()), chunk_loader::load_chunk(&pos));
+    let result = world.get_chunk(&pos);
+    dbg!(result);
+}
+
+#[test]
+fn test_adding_chunks() {
+    let mut world = world::World::new();
+    let pos = ChunkPos(Vector3::<isize>::new(0, 0, 0));
+    let blocks = terrain_generator::generate(&pos);
+    let chunk = Chunk::new(blocks);
+
+    world.add_chunk(ChunkPos(pos.clone()), chunk.clone());
+
+    let chunk2 = world.get_chunk_cloned(&pos).expect("cannot get chunk from world");
+    assert_eq!(chunk, chunk2);
+}
