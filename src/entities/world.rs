cb7861c4bb22b413b2336da4d32894358823b2ce
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-05-02 13:07:19 +0200

    refac: Custom error for World::get_block.

--- src/entities/world.rs
+++ src/entities/world.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, fmt};
 
 use super::{chunk::Chunk, types::{BlockID, BlockPos, ChunkPos}, BlockInChunkPos};
 
@@ -19,14 +19,31 @@ impl World {
     pub fn get_chunk(&self, pos: ChunkPos) -> Option<&Chunk> {
         self.chunks.get(&pos)
     }
-    pub fn get_block(&self, world_pos: BlockPos) -> Result<BlockID, &str> {
+    pub fn get_block(&self, world_pos: BlockPos) -> Result<BlockID, GetBlockErr> {
         if world_pos.z < 0 {
-            return Err("Block outside of the world.");
+            return Err(GetBlockErr::OutsideOfWorld);
         }
 
         let chunk_pos = ChunkPos::from(world_pos);
         let in_chunk_pos = BlockInChunkPos::from(world_pos);
 
-        Ok(self.get_chunk(chunk_pos).ok_or("Block outside of the world")?.get_block_storage().get_block(in_chunk_pos))
+        Ok(self.get_chunk(chunk_pos).ok_or(GetBlockErr::OutsideOfWorld)?.get_block_storage().get_block(in_chunk_pos))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum GetBlockErr {
+    OutsideOfWorld
+}
+
+impl Error for GetBlockErr {}
+
+impl fmt::Display for GetBlockErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            GetBlockErr::OutsideOfWorld => "Block is outside of the world.",
+        };
+
+        write!(f, "{}", s)
     }
 }
