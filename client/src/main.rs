bea5d72cedfe5e2e570b6e8f3d06de53919d2e9f
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-05-05 00:55:52 +0200

    feat: Test VoxelMesher.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,14 @@
+use std::rc::Rc;
+
 use bevy::{
     app::{App, Startup},
     prelude::*,
     DefaultPlugins,
 };
 use controller::ControllerPlugin;
+use chunk_builder::*;
+use shared::entities::{world, BlockSide, BlockType, Chunk, ChunkPos};
+use shared::chunk_loader::*;
 
 mod chunk_builder;
 mod controller;
@@ -25,15 +30,33 @@ fn init_level(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn((
-        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(2.5)))),
-        MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
-        Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
-        GlobalTransform::default(),
-    ));
-
-    commands.spawn((
         PointLight { ..default() },
         Transform::from_xyz(10.0, 20.0, 4.0),
         GlobalTransform::default(),
     ));
+
+    let mut chunk_loader = ChunkLoader::default();
+    let mut world = world::World::new();
+    let pos = ChunkPos::new(0, 0, 0);
+
+    world.add_chunk(pos, chunk_loader.load_chunk(pos));
+
+    match world.get_chunk(pos) {
+        Some(chunk) => build_chunk(chunk),
+        None => (),
+    }
+}
+
+fn build_chunk(chunk: &Chunk) {
+    let air = BlockType::new("air", false);
+    let air = MeshBlockTypeBuilder::new(air).build();
+    let dirt = BlockType::new("dirt", true);
+    let dirt = MeshBlockTypeBuilder::new(dirt).visible(true).texture(BlockSide::Front, "dirt").build();
+
+    let mut block_type_storage = BlockTypeStorage::new();
+    block_type_storage.set_block_type(0, air);
+    block_type_storage.set_block_type(1, dirt);
+
+    let texture_dictionary = Rc::new(TextureDictionary::new());
+    VoxelMesher::new(chunk.get_block_storage().clone(), Rc::new(block_type_storage), texture_dictionary);
 }
