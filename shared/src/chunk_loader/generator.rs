7405ef52295f652436ac082444a36a049d2e0870
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-05-05 16:02:07 +0200

    refac: Change generator to generate only 2 types.

--- shared/src/chunk_loader/generator.rs
+++ shared/src/chunk_loader/generator.rs
@@ -60,7 +60,7 @@ impl Generator {
             }
         */
         if world_z < generated_height {
-            self.noise.gen_range(1..9) as BlockID
+            self.noise.gen_range(1..2) as BlockID
         } else {
             0
         }
