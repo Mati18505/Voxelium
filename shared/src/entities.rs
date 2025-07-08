25b2cb7d198ef5fb179594278d0848a713f5d8d6
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-08 23:08:04 +0200

    refac: rename files.

--- shared/src/entities.rs
+++ shared/src/entities.rs
@@ -1,13 +1,13 @@
 pub mod block_storage;
 pub mod block_type;
-pub mod block_types;
+pub mod block_type_storage;
 pub mod chunk;
 pub mod types;
 pub mod world;
 
 pub use block_storage::*;
 pub use block_type::*;
-pub use block_types::*;
+pub use block_type_storage::*;
 pub use chunk::*;
 pub use types::*;
 pub use world::*;
