54b0c4777b75f56de282856742974e66d2594dfe
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-09 00:13:05 +0200

    refac: rename file

--- client/src/bevy_resources.rs
+++ client/src/bevy_resources.rs
@@ -1,7 +1,7 @@
 pub mod block_types;
-pub mod mesh_block_types;
+pub mod mesh_block_type_storage_resource;
 pub mod textures_config;
 
 pub use block_types::*;
-pub use mesh_block_types::*;
+pub use mesh_block_type_storage_resource::*;
 pub use textures_config::*;
