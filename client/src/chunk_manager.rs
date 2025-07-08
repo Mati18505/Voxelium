3a9d981fdea4c6edbe8bb09374d7011493c490ed
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-08 17:22:54 +0200

    fix: Update block cursor when updating chunks.
    Create ChunkManager event system.

--- client/src/chunk_manager.rs
+++ client/src/chunk_manager.rs
@@ -1,12 +1,16 @@
 pub mod bevy_async_chunk_builder;
 pub mod bevy_chunk_entities_manager;
+pub mod bevy_event_manager;
 pub mod bevy_plugin;
 pub mod chunk_state;
 pub mod chunk_manager;
 pub mod physical_world;
 
-pub use bevy_async_chunk_builder::*;
-pub use bevy_chunk_entities_manager::*;
 pub use bevy_plugin::*;
+pub use bevy_event_manager::*;
 pub use chunk_state::*;
 pub use chunk_manager::*;
+
+use bevy_async_chunk_builder::*;
+use bevy_chunk_entities_manager::*;
+use physical_world::*;
