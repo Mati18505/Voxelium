976efdc4d1643f5f5c85780bab6dfe657088bea0
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-13 21:14:24 +0200

    fix: add assert for unsupported ChunkState transition

--- client/src/chunk_state_manager/chunk_state_manager.rs
+++ client/src/chunk_state_manager/chunk_state_manager.rs
@@ -184,12 +184,11 @@ impl ChunkManager {
         let prev_state = self.world.get_chunk_state(pos);
 
         if prev_state != new_state {
-            let maybe_transition = chunk_state::get_chunk_transition(prev_state, new_state);
+            let transition = chunk_state::get_chunk_transition(prev_state, new_state);
 
-            if let Some(transition) = maybe_transition {
-                self.apply_transition(pos, transition);
-            }
+            assert!(transition.is_some(), "Unsupported transition in chunk {:?}: {:?} -> {:?}", pos, prev_state, new_state);
 
+            self.apply_transition(pos, transition.unwrap());
             self.world.set_chunk_state(pos, new_state);
         }
     }
