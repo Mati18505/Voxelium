9a4b2e1eea979c55bdf150d75e6a461b30b3c553
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-19 00:48:08 +0200

    chore: remove duplicate files

--- client/src/chunk_manager/bevy_plugin.rs
+++ client/src/chunk_manager/bevy_plugin.rs
@@ -88,7 +88,6 @@ fn update(
         chunk_manager_resources
             .chunk_manager
             .update_controller_pos(new_chunk_pos);
-        dbg!(&chunk_manager_resources.chunk_manager);
     }
 
     chunk_manager_resources.chunk_manager.check_loaded_chunks();
