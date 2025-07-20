60c4f6e063087808487f0213bfbbf061151fac08
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-20 18:56:53 +0200

    feat: first version of terrain generator using simdnoise

--- client/src/chunk_manager/bevy_plugin.rs
+++ client/src/chunk_manager/bevy_plugin.rs
@@ -49,7 +49,7 @@ fn init_chunk_manager(mut commands: Commands, game_resources: Res<GameResources>
     let inner_builder = Box::new(AsyncChunkBuilder::new(Arc::new(voxel_mesher)));
     let chunk_builder = Box::new(VersionedChunkBuilder::<()>::new(inner_builder));
     let mut config = Config::new(10, 9);
-    config.dynamic_vertical_loading = false;
+    config.dynamic_vertical_loading = true;
 
     let (chunk_object_tx, chunk_object_rx) = crossbeam_channel::unbounded::<ChunkObjectEvent>();
     let (event_tx, event_rx) = crossbeam_channel::unbounded::<WorldChunkUpdate>();
--- shared/src/chunk_io/providers/terrain_generator.rs
+++ shared/src/chunk_io/providers/terrain_generator.rs
@@ -1,35 +1,42 @@
-use rand::{rngs::ThreadRng, Rng};
+use cgmath::num_traits::Float;
+use simdnoise::{CellDistanceFunction, NoiseBuilder, FbmSettings};
 
 use crate::entities::{
-    name_to_block_id, BlockID, BlockInChunkPos, BlockStorage, ChunkPos, CHUNK_SIZE,
+    name_to_block_id, BlockID, BlockInChunkPos, BlockPos, BlockStorage, ChunkPos, CHUNK_SIZE
 };
 
 #[derive(Debug, Clone)]
 pub struct TerrainGenerator {
-    engine: ThreadRng,
+    chunk_pos: ChunkPos,
+    height_map: Vec<f32>,
+    density_map: Vec<f32>,
 }
 
 impl TerrainGenerator {
     pub fn new() -> Self {
         Self {
-            engine: rand::rng(),
+            chunk_pos: ChunkPos::new(0, 0, 0),
+            height_map: Vec::default(),
+            density_map: Vec::default(),
         }
     }
 
     pub fn generate_terrain(&mut self, chunk_pos: ChunkPos) -> BlockStorage {
+        self.chunk_pos = chunk_pos;
+        self.generate_noise();
+
         let mut blocks = BlockStorage::default().get_blocks().to_owned();
 
         for z in 0..CHUNK_SIZE {
-            for x in 0..CHUNK_SIZE {
-                let world_x: isize = x as isize + chunk_pos.x;
-                let world_z: isize = z as isize + chunk_pos.z;
-
-                let generated_height: i64 = self.generate_height(world_x, world_z);
-
-                for y in 0..CHUNK_SIZE {
+            for y in 0..CHUNK_SIZE {
+                for x in 0..CHUNK_SIZE {
+                    let world_x: isize = x as isize + chunk_pos.x;
                     let world_y: isize = y as isize + chunk_pos.y;
-                    let block_id = self.generate_voxel(world_y as i64, generated_height);
+                    let world_z: isize = z as isize + chunk_pos.z;
+
                     let pos = BlockInChunkPos::new(x, y, z);
+                    let world_pos = BlockPos::new(world_x, world_y, world_z);
+                    let block_id = self.generate_voxel(pos, world_pos);
 
                     blocks[pos.index()] = block_id;
                 }
@@ -38,27 +45,31 @@ impl TerrainGenerator {
 
         BlockStorage::new(blocks)
     }
+    
+    fn generate_noise(&mut self) {
+        let offset_x = self.chunk_pos.x as f32;
+        let offset_y = self.chunk_pos.y as f32;
+        let offset_z = self.chunk_pos.z as f32;
 
-    fn generate_height(&mut self, _world_x: isize, _world_z: isize) -> i64 {
-        self.engine.random_range(5..16)
+        self.density_map = NoiseBuilder::fbm_3d_offset(offset_x, CHUNK_SIZE, offset_y, CHUNK_SIZE, offset_z, CHUNK_SIZE)
+            .with_freq(0.5)
+            .with_octaves(5)
+            .with_seed(1337)
+            .with_lacunarity(0.5)
+            .generate_scaled(0.0, 100.0);
     }
 
-    fn generate_voxel(&mut self, world_y: i64, generated_height: i64) -> BlockID {
-        /*
-            match world_z {
-                world_z if world_z > generated_height => biome.atmosphereBlock,
-                world_z if world_z == generated_height => biome.layer1stBlock,
-                world_z if world_z >= generated_height - 5 => biome.layer2ndBlock,
-                world_z if world_z < generated_height => biome.layer3rdBlock,
-                _ => 0
-            }
-        */
-        if world_y.abs() < generated_height {
-            //self.noise.gen_range(4..5) as BlockID
+    fn generate_voxel(&self, pos: BlockInChunkPos, world_pos: BlockPos) -> BlockID {
+        if self.density_map[pos.index()] < 20.0 {
+            name_to_block_id("stone")
+        } else if self.density_map[pos.index()] < 40.0 {
             name_to_block_id("grass")
         } else {
             name_to_block_id("air")
         }
+    }
+    fn index(pos: BlockInChunkPos) -> usize {
+        pos.x + pos.z * CHUNK_SIZE + pos.y * CHUNK_SIZE * CHUNK_SIZE
     }
 }
 
