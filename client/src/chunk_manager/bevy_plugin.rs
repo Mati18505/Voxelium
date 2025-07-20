742bdbbca4bf7851d2ecd26582ef3971d029f75b
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-20 23:00:17 +0200

    feat: add ChunkManagerResources

--- client/src/chunk_manager/bevy_plugin.rs
+++ client/src/chunk_manager/bevy_plugin.rs
@@ -2,14 +2,14 @@ use std::sync::Arc;
 
 use bevy::prelude::*;
 use shared::{
-    chunk_io::{providers::generated_chunk_provider::GeneratedChunkProvider, ChunkLoader},
+    chunk_io::{providers::generated_chunk_provider::GeneratedChunkProvider, ChunkLoader, ChunkProvider},
     entities::{BlockPos, ChunkPos},
 };
 
 use crate::{
     bevy_render::VoxelMaterial,
     bevy_types::{AppStates, GameResources},
-    chunk_manager::{ChunkObjectEvent, WorldChunkUpdate},
+    chunk_manager::{ChunkManagerSystemsPlugin, ChunkObjectEvent, WorldChunkUpdate},
     chunk_mesh_builder::{
         builders::{
             async_chunk_builder::AsyncChunkBuilder, versioned_chunk_builder::VersionedChunkBuilder,
@@ -21,52 +21,43 @@ use crate::{
 
 use super::{
     bevy_event_manager::WorldChunkUpdateEvent, ChunkEntitiesManager, ChunkManager, Config,
-    EventManager,
 };
 
 pub struct ChunkManagerPlugin;
 impl Plugin for ChunkManagerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppStates::InGame), init_chunk_manager)
+        app.add_systems(OnEnter(AppStates::InGame), init__manager)
             .add_event::<WorldChunkUpdateEvent>()
-            .add_systems(Update, update.run_if(in_state(AppStates::InGame)));
+            .add_systems(Update, update.run_if(in_state(AppStates::InGame)))
+            .insert_resource(ChunkManagerResources::default());
     }
 }
 
 #[derive(Resource)]
-pub struct ChunkManagerResources {
-    pub chunk_manager: ChunkManager,
+pub struct ManagerResources {
     chunk_entities_manager: ChunkEntitiesManager,
-    event_manager: EventManager,
 }
 
-fn init_chunk_manager(mut commands: Commands, game_resources: Res<GameResources>) {
+fn init_manager(mut commands: Commands, game_resources: Res<GameResources>) {
     let voxel_mesher = NaiveMesher::new(
         game_resources.block_type_storage.clone(),
         game_resources.texture_dictionary.clone(),
     );
 
-    let inner_builder = Box::new(AsyncChunkBuilder::new(Arc::new(voxel_mesher)));
-    let chunk_builder = Box::new(VersionedChunkBuilder::<()>::new(inner_builder));
     let mut config = Config::new(10, 9);
     config.dynamic_vertical_loading = false;
 
-    let (chunk_object_tx, chunk_object_rx) = crossbeam_channel::unbounded::<ChunkObjectEvent>();
-    let (event_tx, event_rx) = crossbeam_channel::unbounded::<WorldChunkUpdate>();
-    let chunk_loader_provider = Box::new(GeneratedChunkProvider::new());
-    let chunk_loader = ChunkLoader::new(chunk_loader_provider);
+    let chunk_provider = Box::new(GeneratedChunkProvider::new());
 
-    let mut chunk_manager = ChunkManager::new(chunk_loader, chunk_builder, config);
+    let chunk_manager_resource = ChunkManagerResources::new(voxel_mesher, chunk_provider, config);
 
-    chunk_manager.set_chunk_object_tx(Some(chunk_object_tx));
-    chunk_manager.set_event_tx(Some(event_tx));
+    commands.insert_resource(resource);
 
-    let chunk_manager_resources = ChunkManagerResources {
-        chunk_manager,
+    let manager_resources = ManagerResources {
         chunk_entities_manager: ChunkEntitiesManager::new(chunk_object_rx),
-        event_manager: EventManager::new(event_rx),
     };
-    commands.insert_resource(chunk_manager_resources);
+    
+    commands.insert_resource(manager_resources);
 }
 
 fn update(
@@ -104,4 +95,60 @@ fn update(
     chunk_manager_resources
         .event_manager
         .process_pending(chunk_manager_events);
+}
+
+
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Config {
+    /// Horizontal radius (in chunks) within which chunks are loaded.
+    pub load_distance: usize,
+    /// Horizontal radius (in chunks) within which chunks are rendered.
+    pub render_distance: usize,
+    /// If true, the engine dynamically loads chunks above and below the player based on vertical position.
+    pub dynamic_vertical_loading: bool,
+}
+
+impl Config {
+    pub fn new(load_distance: usize, render_distance: usize) -> Self {
+        assert!(render_distance <= load_distance);
+
+        Config {
+            load_distance,
+            render_distance,
+            dynamic_vertical_loading: false,
+        }
+    }
+}
+
+pub trait VersionedChunkBuilder<T: Send + Sync + Default>: ChunkBuilder<T> + Versioned<T> {}
+
+impl<T: Send + Sync + Default, U> VersionedChunkBuilder<T> for U where
+    U: ChunkBuilder<T> + Versioned<T>
+{
+}
+
+#[derive(Resource)]
+pub struct ChunkManagerResources {
+    world: PhysicalWorld,
+    chunk_loader: ChunkLoader,
+    chunk_builder: Box<dyn VersionedChunkBuilder<()>>,
+    config: Config,
+}
+
+impl ChunkManagerResources {
+    fn new(voxel_mesher: Arc<dyn VoxelMesher>, chunk_provider: Box<dyn ChunkProvider>, config: Config) {
+        let chunk_loader = ChunkLoader::new(chunk_loader_provider);
+
+        let inner_builder = Box::new(AsyncChunkBuilder::new(Arc::new(voxel_mesher)));
+        let chunk_builder = Box::new(VersionedChunkBuilder::<()>::new(inner_builder));
+
+        Self {
+            world: PhysicalWorld::default(),
+            chunk_loader,
+            chunk_builder,
+            config,
+        }
+    }
 }
