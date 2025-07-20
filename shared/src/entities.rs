86a6691f9be8dafedd9a9510fc0c6d5f5ab1a2f2
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-21 01:35:47 +0200

    feat: add ChunkPos generators

--- shared/src/entities.rs
+++ shared/src/entities.rs
@@ -3,6 +3,7 @@ pub mod block_storage;
 pub mod block_type;
 pub mod block_type_storage;
 pub mod chunk;
+pub mod generators;
 pub mod types;
 pub mod world;
 
@@ -11,5 +12,6 @@ pub use block_storage::*;
 pub use block_type::*;
 pub use block_type_storage::*;
 pub use chunk::*;
+pub use generators::*;
 pub use types::*;
 pub use world::*;
