760350462639106e277c8b203f47072c44ac3f57
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-21 03:52:37 +0200

    feat: add state_manager system

--- client/src/chunk_manager/bevy_plugin.rs
+++ client/src/chunk_manager/bevy_plugin.rs
@@ -181,6 +181,7 @@ fn update_controller_pos(
     }
 }
 
+/// Manages loaded chunks in world based on player position.
 fn chunk_streamer(
     mut chunk_pos_changed_ev: EventReader<ChunkPosChangedEvent>,
     mut world: ResMut<PhysicalWorldResource>,
@@ -190,6 +191,7 @@ fn chunk_streamer(
 
     for ev in chunk_pos_changed_ev.read() {
         info!("chunk pos: {:?}", ev.chunk_pos);
+
         let player_pos = ev.chunk_pos;
 
         // Remove all chunks that are still empty.
@@ -261,3 +263,51 @@ fn chunks_builder(
     }
 }
 
+fn is_within_distance(controller_pos: ChunkPos, config: &Config, pos: ChunkPos, distance_in_chunks: usize) -> bool {
+    match config.dynamic_vertical_loading {
+        true => pos.is_within_distance(controller_pos, distance_in_chunks),
+        false => pos.is_within_distance_2d(controller_pos, distance_in_chunks),
+    }
+}
+
+fn create_chunk_status(&self, pos: ChunkPos) -> ChunkStatus {
+    let is_within_render = self.is_within_distance(pos, self.config.render_distance);
+    let is_within_load = self.is_within_distance(pos, self.config.load_distance);
+    let loaded = self.world.get_chunk(pos).is_some();
+    let mesh_built = self.chunk_builder.is_chunk_with_latest_version_built(pos);
+    let needs_rebuild = self.world.get_chunk_need_rebuild(pos);
+
+    ChunkStatus {
+        is_within_render,
+        is_within_load,
+        loaded,
+        mesh_built,
+        needs_rebuild,
+    }
+}
+
+
+/// Adds loaded chunks and built meshes to world.
+fn state_manager(
+    mut world: ResMut<PhysicalWorldResource>,
+    mut chunk_loaded_ev: EventReader<ChunkLoaded>,
+    mut chunk_built_ev: EventReader<ChunkBuilt>,
+) {
+    let world = &mut world.world;
+    
+    for ev in chunk_loaded_ev.read() {
+        let pos = ev.chunk_pos;
+        let chunk = &ev.chunk;
+
+        world.set_chunk(pos, chunk);
+        update_chunk_state(pos);
+    }
+
+    for ev in chunk_built_ev.read() {
+        let pos = ev.chunk_pos;
+        let mesh = &ev.chunk_mesh;
+
+        world.add_chunk_mesh(pos, mesh);
+        update_chunk_state(pos);
+    }
+}
