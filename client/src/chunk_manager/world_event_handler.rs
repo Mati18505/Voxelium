7b9ec486aa891029146ef3894247ab8dedbc8303
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-24 18:56:47 +0200

    refactor: move chunk_state_manager to world_event_handler

--- client/src/chunk_manager/world_event_handler.rs
+++ client/src/chunk_manager/world_event_handler.rs
@@ -17,6 +17,7 @@ impl Plugin for WorldEventHandlerPlugin {
                 Update,
                 (
                     process_world_events.run_if(in_state(AppStates::InGame)),
+                    process_state_update_requests.run_if(in_state(AppStates::InGame)),
                     process_transition_events.run_if(in_state(AppStates::InGame)),
                 ),
             );
@@ -230,6 +231,28 @@ fn process_transition_events(
                 world.chunk_meshes.remove(&pos);
                 chunk_entity_req_ev.write(ChunkEntityEvent::Remove(pos));
             }
+        }
+    }
+}
+
+fn process_state_update_requests(
+    mut state_update_req_ev: EventReader<StateUpdateRequest>,
+    mut chunk_state_transition_ev: EventWriter<ChunkStateTransition>,
+) {
+    for ev in state_update_req_ev.read() {
+        let chunk_pos = ev.chunk_pos;
+        let prev_state = ev.curr_state;
+        let next_state = chunk_state::get_next_chunk_state(prev_state, ev.chunk_status);
+        let transition = chunk_state::get_chunk_transition(prev_state, next_state);
+
+        if let Some(transition) = transition {
+            trace!("Chunk transition in chunk {chunk_pos:?}: {prev_state:?} -> {next_state:?}");
+
+            chunk_state_transition_ev.write(ChunkStateTransition {
+                chunk_pos,
+                transition,
+                new_state: next_state,
+            });
         }
     }
 }
