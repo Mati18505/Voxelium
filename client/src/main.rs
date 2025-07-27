5acb71d3503b328234df73afb8f0d1f1b9207788
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-27 23:28:13 +0200

    refactor(vox_importer): change error types

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,15 @@ use std::sync::Arc;
 use bevy::{
     asset::RenderAssetUsages, color::palettes::css::WHITE, pbr::wireframe::{WireframeConfig, WireframePlugin}, prelude::*, reflect::TypeData, render::{
         render_resource::{Extent3d, TextureDimension, TextureFormat}, settings::{RenderCreation, WgpuFeatures, WgpuSettings}, *
-    }
+    },
+    color::palettes::css::WHITE,
+    log,
+    pbr::wireframe::{WireframeConfig, WireframePlugin},
+    prelude::*,
+    render::{
+        settings::{RenderCreation, WgpuFeatures, WgpuSettings},
+        *,
+    },
 };
 use bevy_asset_loader::prelude::*;
 use bevy_common_assets::json::JsonAssetPlugin;
@@ -17,6 +25,7 @@ use chunk_mesh_builder::*;
 use controller::ControllerPlugin;
 use shared::{
     entities::{init_block_names, name_to_block_id, BlockID, BlockPos, BlockTypeStorage},
+    io::vox_importer,
     physics::RaycastResult,
 };
 
@@ -153,6 +162,50 @@ fn create_resources(
     });
 
     init_block_names(server_block_type_storage_asset.into());
+
+    if let Err(err) = vox_importer::import("assets/vox/character/chr_bow.vox") {
+        log::error!("{err}");
+    }
+}
+
+fn create_1d_color_palette(color_palette_2d: Image) -> Image {
+    assert_eq!(color_palette_2d.width(), 256);
+    assert_eq!(color_palette_2d.height(), 1);
+
+    let extend = Extent3d {
+        width: 256,
+        height: 1,
+        ..default()
+    };
+
+    let data = color_palette_2d.data.unwrap();
+    let format = color_palette_2d.texture_descriptor.format;
+
+    Image::new(extend, TextureDimension::D1, data, format, RenderAssetUsages::default())
+}
+
+fn create_material_storage(
+    textured_materials: &mut ResMut<Assets<TexturedCubeMaterial>>,
+    colored_materials: &mut ResMut<Assets<ColoredCubeMaterial>>,
+    opaque_texture: Handle<Image>,
+    color_palette: Handle<Image>,
+) -> Arc<MaterialStorage> {
+    let mut material_storage = MaterialStorage::default();
+
+    let textured_mat = TexturedCubeMaterial {
+        array_texture: opaque_texture,
+    };
+    let textured_mat_handle = textured_materials.add(textured_mat);
+
+    let colored_mat = ColoredCubeMaterial {
+        color_palette,
+    };
+    let colored_mat_handle = colored_materials.add(colored_mat);
+
+    material_storage.add(0, MaterialHandle::TexturedCube(textured_mat_handle));
+    material_storage.add(1, MaterialHandle::ColoredCube(colored_mat_handle));
+
+    Arc::new(material_storage)
 }
 
 fn create_1d_color_palette(color_palette_2d: Image) -> Image {
