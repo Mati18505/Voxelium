7af86352ce700295cb3bcfefa88f4583291f9431
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-07-27 22:32:26 +0200

    style: cargo clippy

--- shared/src/chunk_io/providers/terrain_generator.rs
+++ shared/src/chunk_io/providers/terrain_generator.rs
@@ -2,7 +2,7 @@ use rand::{rngs::ThreadRng, Rng};
 
 use crate::entities::{
     block_in_chunk_pos_generator::BlockInChunkPosGenerator, name_to_block_id, BlockID,
-    BlockInChunkPos, BlockStorage, ChunkPos, CHUNK_SIZE,
+    BlockStorage, ChunkPos, CHUNK_SIZE,
 };
 
 #[derive(Debug, Clone)]
