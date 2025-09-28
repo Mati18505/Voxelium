ad1f1dc94d2f7082ee3df65c00b92ccb8165156d
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-09-28 16:59:01 +0200

    fix: correct order of elements in RenderShapeStorage

--- client/src/bevy_resources/types.rs
+++ client/src/bevy_resources/types.rs
@@ -1,3 +1,5 @@
+use shared::entities::name_to_block_id;
+
 use crate::{bevy_resources::{Dictionary, MaterialHandle, RenderDesc, Storage}, chunk_mesh_builder::{RenderShape, TextureIndex}};
 
 pub type MaterialName = String;
@@ -10,11 +12,22 @@ pub type RenderDescDictionary = Dictionary<BlockTypeName, RenderDesc>;
 pub type MaterialStorage = Storage<MaterialHandle>;
 pub type RenderShapeStorage = Storage<RenderShape>;
 
+/// Creates [`RenderShapeStorage`] from [`RenderDescDictionary`], by compiling each [`RenderShape`] from [`RenderDesc`].
+/// Items in `RenderShapeStorage` are in order defined by `block_registry`.
 pub fn compile_render_desc_dict(render_desc_dict: &RenderDescDictionary, texture_dictionary: &TextureIndexDictionary) -> RenderShapeStorage {
-    let compiled: Vec<RenderShape> = render_desc_dict
+    let mut compiled: Vec<(u8, RenderShape)> = render_desc_dict
         .iter()
-        .map(|(block_type_name, render_desc)| render_desc.compile(texture_dictionary))
+        .map(|(block_type_name, render_desc)| {
+            let block_id = name_to_block_id(&block_type_name);
+            let compiled = render_desc.compile(texture_dictionary);
+
+            (block_id, compiled)
+        })
         .collect();
 
-    RenderShapeStorage::new(compiled)
+    compiled.sort_by_key(|(id, _)| *id);
+
+    let render_shapes = compiled.into_iter().map(|(_, shape)| shape).collect();
+
+    RenderShapeStorage::new(render_shapes)
 }
