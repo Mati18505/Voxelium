608425f779ee17fb876c5bdcfaf91270e1e0146e
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-03-17 14:08:13 +0100

    docs: update

--- client/src/chunk_mesh_builder/meshers/chunk_mesher.rs
+++ client/src/chunk_mesh_builder/meshers/chunk_mesher.rs
@@ -15,14 +15,14 @@ pub type MesherWarnings = HashMap<MesherWarning, u32>;
 
 #[derive(Debug, Default, Clone)]
 pub struct MesherOutput {
-    /// Generated chunk mesh.
+    /// Generated chunk mesh data.
     pub layers: HashMap<MaterialId, ChunkMeshData>,
     /// Non-fatal issues encountered during mesh creation and repetition count.
     pub warnings: MesherWarnings,
 }
 
 pub trait ChunkMesher: Send + Sync + Debug {
-    /// Creates chunk mesh based on its data.
+    /// Creates chunk mesh data based on its data.
     /// Mesh is always created to the end, warnings don't interrupt mesh creation.
     fn create_mesh(&self, chunk: &Chunk) -> MesherOutput;
 }
